dae7a0e74ce3af0c2838f92fb104e3f93719ce14
Kiran Bhat <bhatkiran74@gmail.com>
2024-07-16 22:33:39 +0530

    created CardsRepository

---
+++ src/main/java/com/keto/cards/repository/CardsRepository.java
@@ -0,0 +1,9 @@
+package com.keto.cards.repository;
+
+import com.keto.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards,Long> {
+}
