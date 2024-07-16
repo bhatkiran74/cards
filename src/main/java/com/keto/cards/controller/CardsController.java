957866f8ae6f0bf0f9253a33c41712ad4704d9b8
Kiran Bhat <bhatkiran74@gmail.com>
2024-07-16 23:55:39 +0530

    implemented controller for findCardsDetailsByMobileNumber

--- src/main/java/com/keto/cards/controller/CardsController.java
+++ src/main/java/com/keto/cards/controller/CardsController.java
@@ -2,6 +2,7 @@ package com.keto.cards.controller;
 
 import com.keto.cards.services.ICardsService;
 import com.keto.cards.utils.constants.CardsConstants;
+import com.keto.cards.utils.dto.CardsDto;
 import com.keto.cards.utils.dto.ErrorResponseDto;
 import com.keto.cards.utils.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 /**
  * CardsController.java
  * Author: Kiransing bhat
@@ -55,5 +57,32 @@ public class CardsController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(CardsConstants.STATUS_201,CardsConstants.MESSAGE_201));
+    }
+
+    /**
+     * Endpoint to find card details based on the provided mobile number.
+     *
+     * @param mobileNumber The mobile number associated with the customer's card.
+     * @return ResponseEntity with status 200 (OK) and a response body containing the card details.
+     */
+    @Operation(summary = "Fetch card details", description = "Fetches card details based on the provided mobile number")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Card details fetched successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CardsDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid mobile number format",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class)) }),
+            @ApiResponse(responseCode = "404", description = "Card details not found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class)) }),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class)) })
+    })
+    @GetMapping("/fetch")
+    public ResponseEntity<CardsDto> findCardsDetailsByMobileNumber(@RequestParam String mobileNumber){
+        CardsDto cardsDto = iCardsService.findCardsDetailsByMobileNumber(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
 }
