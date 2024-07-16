be444ce68d669f0bf6250e4bbb1b12f3436b8e5b
Kiran Bhat <bhatkiran74@gmail.com>
2024-07-17 00:30:45 +0530

    implemented controller for updateCards

--- src/main/java/com/keto/cards/controller/CardsController.java
+++ src/main/java/com/keto/cards/controller/CardsController.java
@@ -1,5 +1,6 @@
 package com.keto.cards.controller;
 
+import com.keto.cards.exception.ResourceNotFoundException;
 import com.keto.cards.services.ICardsService;
 import com.keto.cards.utils.constants.CardsConstants;
 import com.keto.cards.utils.dto.CardsDto;
@@ -20,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 /**
  * CardsController.java
  * Author: Kiransing bhat
@@ -85,4 +88,36 @@ public class CardsController {
         CardsDto cardsDto = iCardsService.findCardsDetailsByMobileNumber(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
+
+    /**
+     * Updates the card details based on the provided CardsDto.
+     *
+     * @param cardsDto The DTO containing updated card information.
+     * @return ResponseEntity containing the status and message of the update operation.
+     * @throws ResourceNotFoundException if no card is found with the given card number.
+     */
+    @Operation(summary = "Update card details", description = "Updates card details based on the provided CardsDto object")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Card details updated successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDto.class)) }),
+            @ApiResponse(responseCode = "417", description = "Failed to update card details",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDto.class)) })
+    })
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateCard(@RequestBody CardsDto cardsDto){
+        boolean isUpdated = iCardsService.updateCard(cardsDto);
+
+        if (isUpdated){
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(CardsConstants.STATUS_200, CardsConstants.MESSAGE_200));
+        }else {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDto(CardsConstants.STATUS_417, CardsConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+
+
+
+
 }
