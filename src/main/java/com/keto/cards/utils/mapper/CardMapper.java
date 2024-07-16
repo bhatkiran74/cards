df95d7231f556c6a6972f59b0cca80c6484ab2b7
Kiran Bhat <bhatkiran74@gmail.com>
2024-07-16 23:54:30 +0530

    added CardMapper for mapToCardDto

---
+++ src/main/java/com/keto/cards/utils/mapper/CardMapper.java
@@ -0,0 +1,16 @@
+package com.keto.cards.utils.mapper;
+
+import com.keto.cards.entity.Cards;
+import com.keto.cards.utils.dto.CardsDto;
+
+public class CardMapper {
+    public static CardsDto mapToCardsDto(Cards cards, CardsDto cardsDto) {
+        cardsDto.setCardNumber(cards.getCardNumber());
+        cardsDto.setCardType(cards.getCardType());
+        cardsDto.setMobileNumber(cards.getMobileNumber());
+        cardsDto.setTotalLimit(cards.getTotalLimit());
+        cardsDto.setAvailableAmount(cards.getAvailableAmount());
+        cardsDto.setAmountUsed(cards.getAmountUsed());
+        return cardsDto;
+    }
+}
